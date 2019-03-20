385f823eec986e9a9241377c7b12b62880b0300f
naisi.wns <naisi.wns@antfin.com>
2019-03-21 01:52:38 +0800

    add ObjectUtil and methods of ArrayUtil

--- src/main/java/tuxedo/wheel/array/ArrayUtil.java
+++ src/main/java/tuxedo/wheel/array/ArrayUtil.java
@@ -4,9 +4,13 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
 import java.util.function.IntFunction;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -52,5 +56,21 @@ public class ArrayUtil {
         List<T> list = new ArrayList<T>(array.length);
         Collections.addAll(list, array);
         return list;
+    }
+
+    public static <T> List<T> unmodifiableList(T[] array) {
+        return Collections.unmodifiableList(Arrays.asList(array));
+    }
+
+    public static <T, R> List<R> unmodifiableList(T[] array, Function<? super T, ? extends R> mapper) {
+        return Collections.unmodifiableList(Arrays.asList(array).stream().map(mapper).collect(Collectors.toList()));
+    }
+
+    public static <T> Set<T> unmodifiableSet(T[] array) {
+        return Collections.unmodifiableSet(new HashSet<>(Arrays.asList(array)));
+    }
+
+    public static <T, R> Set<R> unmodifiableSet(T[] array, Function<? super T, ? extends R> mapper) {
+        return Collections.unmodifiableSet(Arrays.asList(array).stream().map(mapper).collect(Collectors.toSet()));
     }
 }
